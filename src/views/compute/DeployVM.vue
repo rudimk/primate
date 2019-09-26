dbf75c30c49ec35f67474d0fb9d63e6b3020d7bd
Florian Symanowski <f.symanowski@ewerk.com>
2019-09-26 17:44:28 +0200

    Fill instance config summary card with real form data.

--- src/views/compute/DeployVM.vue
+++ src/views/compute/DeployVM.vue
@@ -9,7 +9,7 @@
             layout="vertical"
           >
             <a-form-item
-              v-for="(field, fieldIndex) in filteredInputs"
+              v-for="(field, fieldIndex) in filteredParams"
               :key="fieldIndex"
               :label="$t(field.name)"
               :v-bind="field.name"
@@ -49,6 +49,7 @@
               />
             </a-form-item>
             <div class="card-footer">
+              <!-- ToDo extract as component -->
               <a-button @click="() => this.$router.back()">{{ this.$t('cancel') }}</a-button>
               <a-button type="primary" @click="handleSubmit">{{ this.$t('submit') }}</a-button>
             </div>
@@ -57,6 +58,22 @@
       </a-col>
       <a-col :md="24" :lg="8">
         <a-card :bordered="true" :title="this.$t('yourInstance')">
+          <a-list
+            itemLayout="horizontal"
+            :dataSource="instanceConfigListData"
+          >
+            <a-list-item slot="renderItem" slot-scope="item">
+              <!-- ToDo extract as component -->
+              <div class="list-item">
+                <span class="list-item__icon" v-if="item.icon">
+                  <font-awesome-icon :icon="item.icon"/>
+                </span>
+                <span class="list-item__title">
+                  {{ item.title || '-' }}
+                </span>
+              </div>
+            </a-list-item>
+          </a-list>
         </a-card>
       </a-col>
     </a-row>
@@ -64,6 +81,7 @@
 </template>
 
 <script>
+import Vue from 'vue'
 import { api } from '@/api'
 import store from '@/store'
 import _ from 'lodash'
@@ -86,28 +104,52 @@ export default {
         'serviceofferingid',
         'diskofferingid',
         'rootdisksize'
-      ]
+      ],
+      instanceConfig: []
     }
   },
   computed: {
-    filteredInputs () {
+    filteredParams () {
       return this.visibleParams.map((fieldName) => {
         return this.params.find((param) => fieldName === param.name)
       })
+    },
+    instanceConfigListData () {
+      return _.map(this.instanceConfig, this.getConfig, [])
     }
   },
   watch: {
   },
   beforeCreate () {
-    this.form = this.$form.createForm(this)
+    this.form = this.$form.createForm(this, {
+      onValuesChange: (props, fields) => {
+        this.instanceConfig = { ...this.form.getFieldsValue(), ...fields }
+      }
+    })
   },
   created () {
     this.params = store.getters.apis[this.$route.name]['params']
-    this.params.forEach((param) => {
+    this.filteredParams.forEach((param) => {
       this.fetchOptions(param)
+    })
+    Vue.nextTick().then(() => {
+      this.instanceConfig = this.form.getFieldsValue() // ToDo: maybe initialize with some other defaults
     })
   },
   methods: {
+    getConfig (value, name) {
+      const icon = ['fas', 'memory']
+      const param = _.filter(this.params, (param) => param.name === name)
+      const opts = _.get(param, '0.opts', [])
+      if (opts.length > 0) {
+        value = _.get(opts, `${value}.displaytext`)
+        // icon = ['fab', 'ubuntu'] // ToDo: find icon
+      }
+      return {
+        title: value,
+        icon: icon
+      }
+    },
     handleSubmit () {
       console.log('wizard submit')
     },
@@ -158,10 +200,22 @@ export default {
 </script>
 
 <style lang="less" scoped>
-.card-footer {
-  text-align: right;
-  button + button {
-    margin-left: 8px;
+  .card-footer {
+    text-align: right;
+
+    button + button {
+      margin-left: 8px;
+    }
   }
-}
+
+  .list-item {
+    &__icon {
+      vertical-align: middle;
+      padding-right: 0.5rem;
+    }
+
+    &__title {
+      vertical-align: middle;
+    }
+  }
 </style>
